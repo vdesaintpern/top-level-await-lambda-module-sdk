c0cce51b24d53bc3f1f5c4f16ef7e50d56b8f6ce
Vivien de Saint Pern <vivieper@amazon.com>
2022-05-23 18:38:32 +0200

    Initial commit

---
+++ test/test-secrets-manager-await.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestSecretsManagerAwait from '../lib/test-secrets-manager-await-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test-secrets-manager-await-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestSecretsManagerAwait.TestSecretsManagerAwaitStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
